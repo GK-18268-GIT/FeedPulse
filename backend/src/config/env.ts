d1b274ef085234e6153eb7980b56266a724ede54
GK-18268-GIT <kadg.kavinda2001@gmail.com>
2026-03-10 23:31:21 +0530

    Analyze feedbacks with AI

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -2,18 +2,23 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { PORT, MONGO_DB_URL, CLIENT_URL, ADMIN_EMAIL, ADMIN_PASSWORD, JWT_SECRET_KEY } =
+const { PORT, MONGO_DB_URL, CLIENT_URL, ADMIN_EMAIL, ADMIN_PASSWORD, JWT_SECRET_KEY, GEMINI_API_KEY } =
   process.env;
 
 if (!JWT_SECRET_KEY) {
   throw new Error("JWT_SECRET_KEY environment variable is not defined");
 }
 
+if (!GEMINI_API_KEY) {
+  throw new Error("GEMINI_API_KEY environment variable is not defined");
+}
+
 export const ENV = {
-  PORT: process.env.PORT,
-  MONGO_DB_URL: process.env.MONGO_DB_URL,
-  CLIENT_URL: process.env.CLIENT_URL,
-  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
-  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+  PORT: PORT,
+  MONGO_DB_URL: MONGO_DB_URL,
+  CLIENT_URL: CLIENT_URL,
+  ADMIN_EMAIL: ADMIN_EMAIL,
+  ADMIN_PASSWORD: ADMIN_PASSWORD,
   JWT_SECRET_KEY: JWT_SECRET_KEY,
+  GEMINI_API_KEY: GEMINI_API_KEY
 };
---
+++ backend/src/models/feedback.models.ts
@@ -0,0 +1,104 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IFeedback extends Document {
+    title: string;
+    description: string;
+    category: "Bug" | "Feature Request" | "Improvement" | "Other";
+    status:  "New" | "In Review"| "Resolved";
+
+    submitterName?: string;
+    submitterEmail?: string;
+
+    //AI generated fields
+    ai_category?: "Bug" | "Feature Request" | "Improvement" | "Other";
+    sentiment?: "Positive" | "Neutral" | "Negative";
+    priority_score?: number;
+    summary?: string;
+    tags?: string[];
+
+    processed: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const FeedbackSchema: Schema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            maxlength: 100,
+            trim: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+            maxlength: 1000,
+            trim: true,
+        },
+
+        category: {
+            type: String,
+            enum: ["Bug", "Feature Request", "Improvement", "Other"],
+            required: true,
+        },
+
+        status: {
+            type: String,
+            enum: ["New", "In Review", "Resolved"],
+            default: "New",
+        },
+
+        submitterName: {
+            type: String,
+            trim: true,
+        },
+
+        submitterEmail: {
+            type: String,
+            trim: true,
+            match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"]
+        },
+
+        //AI analysis fields
+        ai_category: {
+            type: String,
+            enum: ["Bug", "Feature Request", "Improvement", "Other"]
+        },
+
+        sentiment: {
+            type: String,
+            enum: ["Positive", "Neutral", "Negative"]
+        },
+
+        priority_score: {
+            type: Number,
+            min: 1,
+            max: 10
+        },
+
+        summary: {
+            type: String
+        },
+
+        tags: {
+            type: [String]
+        },
+
+        processed: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    {
+        timestamps: true
+    }
+);
+
+FeedbackSchema.index({ status: 1 });
+FeedbackSchema.index({ category: 1 });
+FeedbackSchema.index({ priority_score: -1 });
+FeedbackSchema.index({ createdAt: -1 });
+
+export default mongoose.model<IFeedback>("Feedback", FeedbackSchema);
